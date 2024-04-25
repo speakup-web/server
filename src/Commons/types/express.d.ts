838325085af4f19c74133a89250a326025baab91
Ken <kenzie@duck.com>
2024-04-25 09:32:24 +0700

    chore: move Express types from index.d.ts to express.d.ts

---
+++ src/Commons/types/express.d.ts
@@ -0,0 +1,15 @@
+import { type UserRole } from '@Domains/enums/UserRole'
+import { type AwilixContainer } from 'awilix'
+
+declare global {
+  namespace Express {
+    interface Request {
+      container: AwilixContainer
+      isAuthenticated: boolean
+      user: {
+        id: string
+        role: UserRole
+      }
+    }
+  }
+}
