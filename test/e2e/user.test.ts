dcca5a0b4c221d8b9682549072427d5dba6b8070
FajarWicaksono4 <fajarw@it.student.pens.ac.id>
2024-05-07 15:45:46 +0700

    test: add e2e test for user account management

--- test/e2e/user.test.ts
+++ test/e2e/user.test.ts
@@ -268,4 +268,410 @@ describe('/users', () => {
       expect(response.body.data.results).toHaveLength(2)
     })
   })
+
+  describe('GET /users/profile', () => {
+    it('should response 401 when get profile as an anonymous', async () => {
+      const response = await request(app).get('/api/users/profile')
+
+      expect(response.status).toEqual(httpStatus.UNAUTHORIZED)
+      expect(response.body).toHaveProperty('status', 'fail')
+      expect(response.body).toHaveProperty('message')
+    })
+
+    it('should response 200 when get profile information successfully', async () => {
+      const admin = new UserBuilder(
+        'Michael',
+        'michael@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      await usersTableTestHelper.addUser(admin)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: admin.email,
+        password: admin.password,
+      })
+
+      const response = await request(app)
+        .get('/api/users/profile')
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+
+      expect(response.status).toEqual(httpStatus.OK)
+      expect(response.body).toHaveProperty('status', 'success')
+      expect(response.body).toHaveProperty('data')
+      expect(response.body.data).toHaveProperty('name')
+      expect(response.body.data).toHaveProperty('email')
+    })
+  })
+
+  describe('PUT /users/profile', () => {
+    it('should response 401 when update profile as an anonymous', async () => {
+      const response = await request(app).put('/api/users/profile')
+
+      expect(response.status).toEqual(httpStatus.UNAUTHORIZED)
+      expect(response.body).toHaveProperty('status', 'fail')
+      expect(response.body).toHaveProperty('message')
+    })
+
+    it('should response 400 when name is invalid', async () => {
+      const admin = new UserBuilder(
+        'Michael',
+        'michael@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      await usersTableTestHelper.addUser(admin)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: admin.email,
+        password: admin.password,
+      })
+
+      const response = await request(app)
+        .put('/api/users/profile')
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+        .send({
+          name: 'abc',
+        })
+
+      expect(response.status).toEqual(httpStatus.BAD_REQUEST)
+      expect(response.body).toHaveProperty('status', 'fail')
+      expect(response.body).toHaveProperty('message')
+    })
+
+    it('should response 200 when edit profile successfully without sending payload', async () => {
+      const admin = new UserBuilder(
+        'Michael',
+        'michael@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      await usersTableTestHelper.addUser(admin)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: admin.email,
+        password: admin.password,
+      })
+
+      const response = await request(app)
+        .put('/api/users/profile')
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+
+      expect(response.status).toEqual(httpStatus.OK)
+      expect(response.body).toHaveProperty('status', 'success')
+      expect(response.body).toHaveProperty('data')
+      expect(response.body.data).toHaveProperty('userId')
+    })
+
+    it('should response 200 when edit profile successfully', async () => {
+      const admin = new UserBuilder(
+        'Michael',
+        'michael@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      await usersTableTestHelper.addUser(admin)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: admin.email,
+        password: admin.password,
+      })
+
+      const response = await request(app)
+        .put('/api/users/profile')
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+        .send({
+          name: 'Michael Doe',
+          password: 'new_secret_password',
+        })
+
+      expect(response.status).toEqual(httpStatus.OK)
+      expect(response.body).toHaveProperty('status', 'success')
+      expect(response.body).toHaveProperty('data')
+      expect(response.body.data).toHaveProperty('userId')
+    })
+  })
+
+  describe('DELETE /users/task-force/{userEmail}', () => {
+    it('should response 401 when delete taskforce account as an anonymous', async () => {
+      const response = await request(app).delete('/api/users/task-force/non.existing@mail.com')
+
+      expect(response.status).toEqual(httpStatus.UNAUTHORIZED)
+      expect(response.body).toHaveProperty('status', 'fail')
+      expect(response.body).toHaveProperty('message')
+    })
+
+    it('should response 403 when delete taskforce account as taskforce', async () => {
+      const taskforce1 = new UserBuilder(
+        'John Doe',
+        'john@mail.com',
+        'secret_password',
+        UserRole.TASKFORCE,
+      ).build()
+      const taskforce2 = new UserBuilder(
+        'Jane Doe',
+        'jane@mail.com',
+        'secret_password',
+        UserRole.TASKFORCE,
+      ).build()
+      await usersTableTestHelper.addUser(taskforce1)
+      await usersTableTestHelper.addUser(taskforce2)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: taskforce1.email,
+        password: taskforce1.password,
+      })
+
+      const response = await request(app)
+        .delete(`/api/users/task-force/${taskforce2.email}`)
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+
+      expect(response.status).toEqual(httpStatus.FORBIDDEN)
+      expect(response.body).toHaveProperty('status', 'fail')
+      expect(response.body).toHaveProperty('message')
+    })
+
+    it('should response 404 when delete non-existing account', async () => {
+      const admin = new UserBuilder(
+        'Michael',
+        'michael@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      await usersTableTestHelper.addUser(admin)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: admin.email,
+        password: admin.password,
+      })
+
+      const response = await request(app)
+        .delete('/api/users/task-force/non.existing@mail.com')
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+
+      expect(response.status).toEqual(httpStatus.NOT_FOUND)
+      expect(response.body).toHaveProperty('status', 'fail')
+      expect(response.body).toHaveProperty('message')
+    })
+
+    it('should response 400 when delete admin account', async () => {
+      const admin1 = new UserBuilder(
+        'Michael',
+        'michael@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      const admin2 = new UserBuilder(
+        'John Doe',
+        'john@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      await usersTableTestHelper.addUser(admin1)
+      await usersTableTestHelper.addUser(admin2)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: admin1.email,
+        password: admin1.password,
+      })
+
+      const response = await request(app)
+        .delete(`/api/users/task-force/${admin2.email}`)
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+
+      expect(response.status).toEqual(httpStatus.BAD_REQUEST)
+      expect(response.body).toHaveProperty('status', 'fail')
+      expect(response.body).toHaveProperty('message')
+    })
+
+    it('should response 200 when delete taskforce account successfully', async () => {
+      const admin = new UserBuilder(
+        'Michael',
+        'michael@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      const taskforce = new UserBuilder(
+        'John Doe',
+        'john@mail.com',
+        'secret_password',
+        UserRole.TASKFORCE,
+      ).build()
+      await usersTableTestHelper.addUser(admin)
+      await usersTableTestHelper.addUser(taskforce)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: admin.email,
+        password: admin.password,
+      })
+
+      const response = await request(app)
+        .delete(`/api/users/task-force/${taskforce.email}`)
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+
+      expect(response.status).toEqual(httpStatus.OK)
+      expect(response.body).toHaveProperty('status', 'success')
+      expect(response.body).toHaveProperty('data')
+      expect(response.body.data).toHaveProperty('userId')
+    })
+  })
+
+  describe('PUT /users/task-force/{userEmail}', () => {
+    it('should response 401 when edit taskforce account as an anonymous', async () => {
+      const response = await request(app).put('/api/users/task-force/non.existing@mail.com')
+
+      expect(response.status).toEqual(httpStatus.UNAUTHORIZED)
+      expect(response.body).toHaveProperty('status', 'fail')
+      expect(response.body).toHaveProperty('message')
+    })
+
+    it('should response 403 when edit taskforce account as taskforce', async () => {
+      const taskforce1 = new UserBuilder(
+        'John Doe',
+        'john@mail.com',
+        'secret_password',
+        UserRole.TASKFORCE,
+      ).build()
+      const taskforce2 = new UserBuilder(
+        'Jane Doe',
+        'jane@mail.com',
+        'secret_password',
+        UserRole.TASKFORCE,
+      ).build()
+      await usersTableTestHelper.addUser(taskforce1)
+      await usersTableTestHelper.addUser(taskforce2)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: taskforce1.email,
+        password: taskforce1.password,
+      })
+
+      const response = await request(app)
+        .put(`/api/users/task-force/${taskforce2.email}`)
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+
+      expect(response.status).toEqual(httpStatus.FORBIDDEN)
+      expect(response.body).toHaveProperty('status', 'fail')
+      expect(response.body).toHaveProperty('message')
+    })
+
+    it('should response 404 when edit non-existing account', async () => {
+      const admin = new UserBuilder(
+        'Michael',
+        'michael@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      await usersTableTestHelper.addUser(admin)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: admin.email,
+        password: admin.password,
+      })
+
+      const response = await request(app)
+        .put('/api/users/task-force/non.existing@mail.com')
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+
+      expect(response.status).toEqual(httpStatus.NOT_FOUND)
+      expect(response.body).toHaveProperty('status', 'fail')
+      expect(response.body).toHaveProperty('message')
+    })
+
+    it('should response 400 when edit admin account', async () => {
+      const admin1 = new UserBuilder(
+        'Michael',
+        'michael@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      const admin2 = new UserBuilder(
+        'John Doe',
+        'john@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      await usersTableTestHelper.addUser(admin1)
+      await usersTableTestHelper.addUser(admin2)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: admin1.email,
+        password: admin1.password,
+      })
+
+      const response = await request(app)
+        .put(`/api/users/task-force/${admin2.email}`)
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+
+      expect(response.status).toEqual(httpStatus.BAD_REQUEST)
+      expect(response.body).toHaveProperty('status', 'fail')
+      expect(response.body).toHaveProperty('message')
+    })
+
+    it('should response 200 when edit taskforce account without payload', async () => {
+      const admin = new UserBuilder(
+        'Michael',
+        'michael@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      const taskforce = new UserBuilder(
+        'John Doe',
+        'john@mail.com',
+        'secret_password',
+        UserRole.TASKFORCE,
+      ).build()
+      await usersTableTestHelper.addUser(admin)
+      await usersTableTestHelper.addUser(taskforce)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: admin.email,
+        password: admin.password,
+      })
+
+      const response = await request(app)
+        .put(`/api/users/task-force/${taskforce.email}`)
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+
+      expect(response.status).toEqual(httpStatus.OK)
+      expect(response.body).toHaveProperty('status', 'success')
+      expect(response.body).toHaveProperty('data')
+      expect(response.body.data).toHaveProperty('userId')
+    })
+
+    it('should response 200 when edit taskforce account successfuly', async () => {
+      const admin = new UserBuilder(
+        'Michael',
+        'michael@mail.com',
+        'secret_password',
+        UserRole.ADMIN,
+      ).build()
+      const taskforce = new UserBuilder(
+        'John Doe',
+        'john@mail.com',
+        'secret_password',
+        UserRole.TASKFORCE,
+      ).build()
+      await usersTableTestHelper.addUser(admin)
+      await usersTableTestHelper.addUser(taskforce)
+
+      const loginResponse = await request(app).post('/api/auth/login').send({
+        email: admin.email,
+        password: admin.password,
+      })
+
+      const response = await request(app)
+        .put(`/api/users/task-force/${taskforce.email}`)
+        .set('Authorization', `Bearer ${loginResponse.body.data.accessToken}`)
+        .send({
+          name: 'Johny Doe',
+          password: 'new_secret_password',
+        })
+
+      expect(response.status).toEqual(httpStatus.OK)
+      expect(response.body).toHaveProperty('status', 'success')
+      expect(response.body).toHaveProperty('data')
+      expect(response.body.data).toHaveProperty('userId')
+    })
+  })
 })
